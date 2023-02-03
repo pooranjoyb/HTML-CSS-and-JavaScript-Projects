a3b0196c2429463cab94f3201da26826ca3967a4
pooranjoyb <pooranjoyb2016@gmail.com>
2023-02-04 00:13:36 +0530

    Initialized Train-Tracker project

---
+++ Train-Tracker/README.md
@@ -0,0 +1,2 @@
+# Train-Tracker
+A Nodejs App to track Train information and serve them to the user.
---
+++ Train-Tracker/server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bp from 'body-parser'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 8000
+
+app.set('view engine', 'ejs')
+
+app.use(express.static("static"))
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended: false}))
+
+//home route
+app.get('/', (req, res) => {
+    res.render('../static/views/base', {title: "Train Tracker"});
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
