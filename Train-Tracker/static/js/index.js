50cdf06016e4745a144bc9175c3774117fb8c696
pooranjoyb <pooranjoyb2016@gmail.com>
2023-02-09 20:20:38 +0530

    Adding new elements in DOM using js

---
+++ Train-Tracker/static/js/index.js
@@ -0,0 +1,9 @@
+var addNewElement = () => {
+    var elem = document.getElementById("newElem")
+    var head = document.getElementById("newHead")
+    elem.className = "text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+    head.className = "text-sm font-medium  text-white bg-gray-700 px-6 py-4"
+
+}
+
+// addNewElement()
